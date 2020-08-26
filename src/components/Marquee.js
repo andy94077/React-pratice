03aa50253c1966aea3d0b038f9ff13a7812bfc02
andy.chen <andy.chen@funpodium.net>
2020-08-26 11:48:43 +0800

    Finish irresponsive Marquee

--- src/components/Marquee.js
+++ src/components/Marquee.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./Marquee.css";
+import clsx from "clsx";
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles(() => ({
@@ -10,39 +11,75 @@ const useStyles = makeStyles(() => ({
     position: "relative",
   },
   marqueeContainer: {
+    // minWidth: "100%",
+    // marginRight: "100%",
     paddingLeft: 0,
-    display: "flex",
+    display: "block",
     listStyleType: "none",
     position: "absolute",
   },
   child: {
     whiteSpace: "nowrap",
-    marginRight: "100vw",
+    // width: "100%",
+    // marginRight: "100vw",
   },
-  lastChild: {
-    whiteSpace: "nowrap",
+  pause: {
+    animationPlayState: "paused",
   },
 }));
 
 export default function Marquee(props) {
-  const { children, seconds = 5 } = props;
+  const { children } = props;
   const classes = useStyles();
   const [head, setHead] = useState(0);
+  const [pause, setPause] = useState(false);
+  const liRef = useRef(null);
+  // const ulRef = useRef(null);
+  const [liWidth, setliWidth] = useState(0);
+
+  // useEffect(() => {
+  //   ulRef.current.addEventListener("animationstart", () =>
+  //     console.log("start")
+  //   );
+  //   return () =>
+  //     ulRef.current.removeEventListener("animationstart", () =>
+  //       console.log("remove")
+  //     );
+  // }, [ulRef]);
 
   useEffect(() => {
-    setTimeout(() => {
+    setliWidth(liRef.current.offsetWidth);
+
+    const timer = setTimeout(() => {
+      setPause(true);
       setHead((prev) => (prev + 1) % children.length);
-    }, seconds * 1000);
-  }, [head]);
+    }, ((window.innerWidth + liRef.current.offsetWidth) / 200) * 1000);
+
+    setPause(false);
+    return () => clearTimeout(timer);
+  }, [head, window.innerWidth]);
 
   return (
-    <div className={classes.marquee}>
-      <ul
-        className={classes.marqueeContainer}
-        style={{ animation: `marquee ${seconds}s linear infinite` }}
-      >
-        <li className={classes.lastChild}>{children[head]}</li>
-      </ul>
-    </div>
+    <>
+      <div className={classes.marquee}>
+        <ul
+          key={head}
+          className={clsx(classes.marqueeContainer, { [classes.pause]: pause })}
+          style={{
+            animation: `marquee ${
+              (window.innerWidth + liWidth) / 200
+            }s linear 1`,
+          }}
+          // ref={ulRef}
+        >
+          <li className={classes.child} ref={liRef}>
+            {children[head]}
+          </li>
+        </ul>
+      </div>
+      <p>current speed {(window.innerWidth + liWidth) / 200}</p>
+      <p>li width {liWidth}</p>
+      <p>window width {window.innerWidth}</p>
+    </>
   );
 }
